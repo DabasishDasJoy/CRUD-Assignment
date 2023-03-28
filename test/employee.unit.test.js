4ab7d5df8086294cb25f943a3192e3b047f8a4b6
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2023-03-28 11:18:49 +0600

    test cases for delete Employee added

--- test/employee.unit.test.js
+++ test/employee.unit.test.js
@@ -4,7 +4,7 @@ const Employee = require("../schemas/employeeSchema");
 const { updatedEmployee, deleteEmployee } = require("../controllers/employee");
 
 describe("---------------Employee Controller - UNIT Test ---------", () => {
-  // describe block for the updatedEmployee function
+  // describtion for updatedEmployee function
   describe("updatedEmployee", () => {
     // it block for testing if the function updates an employee by id
     it("should update an employee by id", async () => {
@@ -101,6 +101,97 @@ describe("---------------Employee Controller - UNIT Test ---------", () => {
 
       // Restore the stubbed method to its original implementation
       findByIdAndUpdateStub.restore();
+    });
+  });
+
+  // describtion for deleteEmployee function
+  describe("deleteEmployee", () => {
+    it("should delete the employee", async () => {
+      // req object definition
+      const req = {
+        params: { employeeId: "64226271d84a6c25e809b26b" },
+      };
+
+      // res object definition
+      const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
+
+      // create a stub to resolve with empty object
+      const findByIdAndDeleteStub = sinon
+        .stub(Employee, "findByIdAndDelete")
+        .resolves({});
+
+      // Call the "deleteEmployee"  method
+      await deleteEmployee(req, res);
+
+      // Assertion to check for expected response
+      expect(findByIdAndDeleteStub.calledOnceWith("64226271d84a6c25e809b26b"))
+        .to.be.true;
+      expect(res.status.calledOnceWith(200)).to.be.true;
+      expect(
+        res.json.calledOnceWith({ message: "Employee deleted successfully" })
+      ).to.be.true;
+
+      // Restore stubbed method
+      findByIdAndDeleteStub.restore();
+    });
+
+    it("should return 404 if employee is not found", async () => {
+      // Define request parameters
+      const req = {
+        params: { employeeId: "123" },
+      };
+
+      // Define a response object with stubbed methods
+      const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
+
+      // Create a stub for the 'findByIdAndDelete' method of the Employee schema
+      const findByIdAndDeleteStub = sinon.stub(Employee, "findByIdAndDelete");
+
+      // Resolve with null if ID is "123" which is an invalid id
+      findByIdAndDeleteStub.withArgs("123").resolves(null);
+
+      // Resolve with a truthy value for any other ID
+      findByIdAndDeleteStub.resolves({});
+
+      // Call the function being tested
+      await deleteEmployee(req, res);
+
+      // Assertions to check if the response is as expected
+      expect(findByIdAndDeleteStub.calledOnceWith("123")).to.be.true;
+      expect(res.status.calledOnceWith(404)).to.be.true;
+      expect(res.json.calledOnceWith({ message: "Employee not found" })).to.be
+        .true;
+
+      // Restore the stubbed method
+      findByIdAndDeleteStub.restore();
+    });
+
+    it("should return 500 if there is a server error", async () => {
+      // Define mock request object
+      const req = {
+        params: { employeeId: "123" },
+      };
+
+      // Define mock response object with stubbed status and json methods
+      const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
+
+      // Stub the Employee.findByIdAndDelete method to throw an error
+      const findByIdAndDeleteStub = sinon
+        .stub(Employee, "findByIdAndDelete")
+        .throws();
+
+      // Call the tested function with the mock request and response objects
+      await deleteEmployee(req, res);
+
+      // Verify that the stubbed method was called with the correct arguments
+      expect(findByIdAndDeleteStub.calledOnceWith("123")).to.be.true;
+
+      // Verify that the response status and json methods were called with the correct arguments
+      expect(res.status.calledOnceWith(500)).to.be.true;
+      expect(res.json.calledOnceWith({ message: "Server Error" })).to.be.true;
+
+      // Restore the stubbed method to its original implementation
+      findByIdAndDeleteStub.restore();
     });
   });
 });
